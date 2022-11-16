13650569d2804d366d20a54900c36b385063323a
Victor Barros <coffiekingb@gmail.com>
2022-11-15 22:19:57 -0300

    :sparkles: Feat: Create Sign-Up Service

---
+++ src/Services/authService.ts
@@ -0,0 +1,22 @@
+import crypt from "../utils/crypt";
+import { IUserData } from "../Types/authTypes";
+import tokenFunctions from "../utils/tokenFunctions";
+import authRepository from "../Repository/authRepository";
+
+async function createUser(userData:IUserData){
+  if(userData.confirmPassword!==userData.password){
+    throw{code:"Bad_Request", message:"Passwords Is Not Same."}
+  }
+  const userExist = await authRepository.getByEmail(userData.email)
+  if (userExist) {
+    throw { code: "Conflict", message: "User Already Exist." }
+  }
+  const passwordEncripted = crypt.encriptByHash(userData.password);
+  const createUser = {name:userData.name,email:userData.email,password:passwordEncripted};
+  const userCreated = await authRepository.insertUser(createUser);
+  return {email:userCreated.email,name:userCreated.name}
+}
+
+export default {
+  createUser
+}
