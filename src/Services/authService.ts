c9261b0c6be94b7ea947d02dfe4644de7b12c057
Victor Barros <coffiekingb@gmail.com>
2022-11-15 23:48:38 -0300

    :sparkles: Feat: Create Sign-in Service

--- src/Services/authService.ts
+++ src/Services/authService.ts
@@ -1,5 +1,5 @@
 import crypt from '../utils/crypt';
-import { IUserData } from '../Types/authTypes';
+import { ILogin, IUserData } from '../Types/authTypes';
 import tokenFunctions from '../utils/tokenFunctions';
 import authRepository from '../Repository/authRepository';
 
@@ -7,10 +7,12 @@ async function createUser(userData: IUserData) {
   if (userData.confirmPassword !== userData.password) {
     throw { code: 'Bad_Request', message: 'Passwords Is Not Same.' };
   }
+
   const userExist = await authRepository.getByEmail(userData.email);
   if (userExist) {
     throw { code: 'Conflict', message: 'User Already Exist.' };
   }
+
   const passwordEncripted = crypt.encriptByHash(userData.password);
   const createUser = {
     name: userData.name,
@@ -21,6 +23,21 @@ async function createUser(userData: IUserData) {
   return { email: userCreated.email, name: userCreated.name };
 }
 
+async function login(loginData: ILogin) {
+  const user = await authRepository.getByEmail(loginData.email);
+  if (!user) {
+    throw { code: 'Not_Found', message: 'Email or Password is Incorrect.' };
+  }
+  const isPassword = crypt.compareHash(loginData.password, user.password);
+  if (!isPassword) {
+    throw { code: 'Not_Found', message: 'Email or Password is Incorrect.' };
+  }
+  const token = tokenFunctions.createJWT({ id: user.id }, { expiresIn: '1h' });
+
+  return { email: user.email, name: user.name, token };
+}
+
 export default {
-  createUser
+  createUser,
+  login
 };
