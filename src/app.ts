b4b1ee674f77f6c5b4249276216218cf965d652f
Victor Barros <coffiekingb@gmail.com>
2022-11-15 22:22:31 -0300

    :sparkles: Feat: Create  App

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-import express, {json} from "express"
-import cors from "cors"
-import "express-async-errors"
-import router from "./router"
-const app =  express()
-app.use(cors())
-app.use(json())
-app.use(router)
-export default app
+import express, {json} from "express";
+import cors from "cors";
+import "express-async-errors";
+import router from "./router";
+import errorHandle from "./middleware/erroHandle";
+const app =  express();
+app.use(cors());
+app.use(json());
+app.use(router);
+app.use(errorHandle)
+export default app;
