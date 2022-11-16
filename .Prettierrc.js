f676f570311728435726ee851b5503f22cbc4ce3
Victor Barros <coffiekingb@gmail.com>
2022-11-15 22:24:27 -0300

    :lipstick: Style: Create Prettier file

---
+++ .Prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  tabSize: 2,
+  formatOnSave: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  trailingComma: 'none',
+};
