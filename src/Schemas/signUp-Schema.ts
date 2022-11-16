6d765cd3c56b183d0d704ff9e9e96d28a77fb10c
Victor Barros <coffiekingb@gmail.com>
2022-11-15 22:44:48 -0300

    :sparkles: Feat: Update Sign-Up Schema

--- src/Schemas/signUp-Schema.ts
+++ src/Schemas/signUp-Schema.ts
@@ -2,8 +2,8 @@ import joi from "joi";
 const signUp = joi.object({
   name: joi.string().required(),
   email: joi.string().email().required(),
-  password: joi.string().required(),
-  confirmPassword: joi.string().required()  
+  password: joi.string().min(6).max(30).required(),
+  confirmPassword: joi.string().min(6).max(30).required()  
 })
 
 export default signUp
